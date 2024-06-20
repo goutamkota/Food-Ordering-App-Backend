cc9f9df079e1202929fbed04e0530f4f98db3470
kgoutam <goutam.s@iserveu.co.in>
2024-06-20 22:34:21 +0530

    create restaurant backend api done

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary"
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => console.log("Connected MongoDB!"))
     .catch((error : Error) => console.log(error))
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET,
+})
 
 const corsOptions = {
     origin : '*',
@@ -25,6 +33,7 @@ app.get("/health", async (req : Request, res : express.Response) => {
 })
 
 app.use('/api/my/user', myUserRoute)
+app.use('/api/my/restaurant', myRestaurantRoute)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -17,3 +17,15 @@ export const validateMyUserRequest = [
     handleValidationErrors,
 ]
 
+export const validateMyRestaurantRequest = [
+    body("restaurantName").isString().notEmpty().withMessage("Restaurant Name is required"),
+    body("city").isString().notEmpty().withMessage("City number must be required"),
+    body("country").isString().notEmpty().withMessage("Country number must be required"),
+    body("deliveryPrice").isFloat({min: 0}).withMessage("DeliveryPrice must be number"),
+    body("estimatedDeliveryTime").isInt({min:0}).withMessage("Estimated DeliveryTime must be number"),
+    body("cuisines").isArray().withMessage("Cuisine is an array").not().isEmpty().withMessage("Cuisine array must not be empty"),
+    body("menuItems").isArray().withMessage("Menu Items must be an array"),
+    body("menuItems.*.name").notEmpty().withMessage("Menu Item Name is required"),
+    body("menuItems.*.price").isFloat({min: 0}).withMessage("Menu Item Price is required and must be number"),
+    handleValidationErrors,
+]
